0f861ee6c5ea068200e2f3af6f7a474fd6cc0129
Dylan <invision33@gmail.com>
2016-02-01 23:37:59 -0600

    add home and 404 states

--- app/scripts/routes.js
+++ app/scripts/routes.js
@@ -1,1 +1,27 @@
+angular.module('Sundial.config', ['ui.router']).
 
+config( ['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+
+	$urlRouterProvider.
+
+		when('', '/home').
+
+		when('/', '/home').
+
+		otherwise(function($injector) {
+			$injector.get('$state').go('404', {}, { location: false });
+		}); 
+		
+
+	$stateProvider.
+
+		state('home', {
+			url: '/home',
+			templateUrl: 'views/tpl/home.html'
+		}).
+
+		state('404', {
+			templateUrl: 'views/tpl/404.html'
+		}); 
+
+}]); 
--- tests/unit/routes-spec.js
+++ tests/unit/routes-spec.js
@@ -2,21 +2,44 @@ describe('Sundial.routes', function() {
 
 	var $state,
 			$rootScope,
-			testState;
+			testRoute;
+
+	function goTo(url) {
+		$location.url(url);
+		$rootScope.$digest();
+	}
 
 	beforeEach(module('Sundial')); 
 
-	beforeEach(inject(function(_$state_, $templateCache, _$rootScope_) {
+	beforeEach(inject(function(_$state_, $templateCache, _$rootScope_, _$location_) {
 		$state = _$state_;
 		$rootScope = _$rootScope_; 
 		$templateCache.put('views/tpl/home.html', '');
+		$templateCache.put('views/tpl/404.html', '');
+		$location = _$location_; 
 	})); 
 
-		it('should redirect to home on blank', function() {
-			testState = 'home'; 
-			$state.go(testState); 
-			$rootScope.$digest(); 
-			expect($state.href(testState)).toBe('#/home')
+	describe('home state', function() {
+
+		it('should go to home state on blank', function() {
+			testRoute = ""; 
+			goTo(testRoute); 
+			expect($state.current.name).toBe('home'); 
 		});
+
+		it('should go to home state on "/"', function() {
+			testRoute = "/"; 
+			goTo(testRoute); 
+			expect($state.current.name).toBe('home'); 
+		});
+	});
+
+	describe('404 state', function() {
+		it('should go to 404 state on bad URL', function() {
+			goTo('aBadUrl');
+			expect($state.current.name).toBe('404'); 
+		})
+	})
+	
 
 })
